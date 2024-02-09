5962425609a1e760bcb58dd18eed493b4f991a8a
Yuskus <yuskus@yandex.ru>
2024-02-09 13:57:16 +0300

    homework for seminar 2 is done, 09.02.24

---
+++ Task3/Program.cs
@@ -0,0 +1,22 @@
+﻿// Задача 3: Напишите программу, которая принимает на вход целое число
+// из отрезка [10, 99] и показывает наибольшую цифру числа.
+
+Console.WriteLine("Впишите число от 10 до 99 включительно:");
+int number = Convert.ToInt32(Console.ReadLine());
+if (number > 9 && number < 100)
+{
+    int leftDigit = number / 10;
+    int rightDigit = number % 10;
+    if (leftDigit >= rightDigit)
+    {
+        Console.WriteLine("Наибольшая цифра числа: " + leftDigit);
+    }
+    else
+    {
+        Console.WriteLine("Наибольшая цифра числа: " + rightDigit);
+    }
+}
+else
+{
+    Console.WriteLine("Ошибка! Введенное число не входит в предполагаемый диапазон.");
+}
---
+++ Task4/Program.cs
@@ -0,0 +1,41 @@
+﻿// Задача 4: Напишите программу, которая на вход принимает 
+// натуральное число N, а на выходе показывает его цифры через запятую.
+//
+// P.S. Решила сделать вывод слева направо и справа налево.
+
+Console.WriteLine("Введите число:");
+int number = Convert.ToInt32(Console.ReadLine());
+int reversedNumber = 0;
+int digit = 0;
+Console.WriteLine("Цифры числа справа налево:");
+while (number > 0)
+{
+    digit = number % 10;
+    Console.Write(digit);
+    reversedNumber += digit;
+    if (number > 9)
+    {
+        reversedNumber *= 10;
+        Console.Write(", ");
+    }
+    else
+    {
+        Console.Write(".\n");
+    }
+    number /= 10;
+}
+Console.WriteLine("Цифры числа слева направо:");
+while (reversedNumber > 0)
+{
+    digit = reversedNumber % 10;
+    Console.Write(reversedNumber % 10);
+    if (reversedNumber > 9)
+    {
+        Console.Write(", ");
+    }
+    else
+    {
+        Console.Write(".");
+    }
+    reversedNumber /= 10;
+}
